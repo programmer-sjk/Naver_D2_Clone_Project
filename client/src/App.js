b9831d83d0a9099d84f4de041de47a59fdaab2d6
sjk5766 <wjdrnr6662@gmail.com>
2020-02-19 23:53:39 +0900

    프로젝트 시작 전, 메인 라우터 기능 개발 중

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from '/images/top_logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        asdasdad
-      </header>
+    <div className="header_section">
+        <img src="/images/top_logo.png" className="main_logo"/>
+        <ul>
+            <li>Hello world</li>
+            <li>D2 News</li>
+            <li>D2 Program</li>
+            <li>About D2</li>
+        </ul>
+        {/*<img className="main_logo"/>*/}
     </div>
   );
 }
