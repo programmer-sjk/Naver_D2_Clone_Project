9fb580be1b3d06c31bde3fb4dc2ae038d12dd6a5
sjk5766 <wjdrnr6662@gmail.com>
2020-03-16 23:39:15 +0900

    메인 로고 라우터 추가

--- client/src/App.js
+++ client/src/App.js
@@ -6,13 +6,15 @@ import Hello from './components/hello'
 import News from './components/news'
 import Program from './components/program'
 import About from './components/about'
+import Home from './components/home'
 
 function App() {
     return (
         <div>
             <div className="header_section">
-                <img src="/images/top_logo.png" className="main_logo"/>
                 <ul>
+                    {/*<li><Link to="/"></Link></li>*/}
+                    <li><Link to="/"><img src="/images/top_logo.png" className="main_logo"/></Link></li>
                     <li><Link to="/hello">Hello world</Link></li>
                     <li><Link to="/news">D2 News</Link></li>
                     <li><Link to="/program">D2 Program</Link></li>
@@ -20,6 +22,8 @@ function App() {
                 </ul>
             </div>
             <div>
+                {/*<Route path="/" component={Home} exact={true}/>*/}
+                <Route path="/" component={Home} exact={true}/>
                 <Route path="/hello" component={Hello}/>
                 <Route path="/news" component={News}/>
                 <Route path="/program" component={Program}/>
