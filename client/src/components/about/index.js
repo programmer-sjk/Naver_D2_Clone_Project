de60b3e8b35c9e9c3386536babb035c16f7473fd
sjk5766 <wjdrnr6662@gmail.com>
2020-04-01 00:11:51 +0900

    About D2 schedule 개발중

--- client/src/components/about/index.js
+++ client/src/components/about/index.js
@@ -7,13 +7,37 @@ const About = () => {
     const title = 'About D2';
     const desc = 'D2가 개발자들과 함께 걸어온 길을 소개합니다.';
 
+    const data = {
+        image: '/images/schedule1.png',
+        title: 'NAVER CAMPUS HACKDAY',
+        text: "NAVER 개발자와 함께하는 24시간의 해커톤",
+    }
+    
     return (
         <div>
             <Description bgColor={bgColor} title={title} desc={desc}></Description>
             <div className="content_area">
                 <div className="about_schedule">
-                    <h2> 2020 주요 일정 </h2>
-                    <button className="btn_history">D2 히스토리 바로가기</button>
+                    <div className="schedule_title">
+                        <h2> 2020 주요 일정 </h2>
+                        <button className="btn_history">D2 히스토리 바로가기</button>
+                    </div>
+                    <div className="schedule_list">
+                        <div className="schedule">
+                            <div>
+                                <div className="schedule_img"><img src={data.image}/></div>
+                                <div className="schedule_txt">
+                                    <p className="title">{data.title}</p>
+                                    <p className="desc">{data.text}</p>
+                                </div>
+                            </div>
+                            <div className="date_info">
+                                <p>20년 3월 18일 ~ 4월 2일 접수</p>
+                                <p>20년 5월 21일 ~ 5월 22일 핵데이</p>
+                                <p>바로가기</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 
             </div>
