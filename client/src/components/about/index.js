e789b5d17f1f8c882455c4b50daf82d543bb4961
sjk5766 <wjdrnr6662@gmail.com>
2020-04-01 19:00:35 +0900

    About D2 schedule 개발완료

--- client/src/components/about/index.js
+++ client/src/components/about/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Description from 'components/common/description'
 import 'css/About.css';
 
@@ -6,13 +6,34 @@ const About = () => {
     const bgColor ='#0090d7';
     const title = 'About D2';
     const desc = 'D2가 개발자들과 함께 걸어온 길을 소개합니다.';
-
-    const data = {
-        image: '/images/schedule1.png',
-        title: 'NAVER CAMPUS HACKDAY',
-        text: "NAVER 개발자와 함께하는 24시간의 해커톤",
-    }
+    const datas = [
+        {
+            image: '/images/schedule1.png',
+            title: 'NAVER CAMPUS HACKDAY',
+            text: "NAVER 개발자와 함께하는 24시간의 해커톤",
+            date: ['20년 3월 18일 ~ 4월 2일 접수', '20년 5월 21일 ~ 5월 22일 핵데이'],
+            link: 'https://recruit.navercorp.com/naver/job/detail/developer?annoId=20003868&classId=&jobId=&entTypeCd=004&searchTxt=&searchSysComCd='
+        },
+        {
+            image: '/images/schedule2.png',
+            title: 'NAVER TECH CONCERT',
+            text: "Mobile 개발을 주제로 기술 컨퍼런스 진행",
+            date: ['20년 7월 예정'],
+            link: 'https://techcon.naver.com/'
+        },
+        {
+            image: '/images/schedule3.png',
+            title: 'DEVIEW',
+            text: "네이버 개발자 컨퍼런스",
+            date: ['20년 9월 예정'],
+            link: 'https://deview.kr/2019'
+        }
+    ]
     
+    const moveHistory = () => {
+        window.scrollTo({top: 887, behavior:'smooth'})
+    }
+
     return (
         <div>
             <Description bgColor={bgColor} title={title} desc={desc}></Description>
@@ -20,26 +41,38 @@ const About = () => {
                 <div className="about_schedule">
                     <div className="schedule_title">
                         <h2> 2020 주요 일정 </h2>
-                        <button className="btn_history">D2 히스토리 바로가기</button>
+                        <button className="btn_history" onClick={moveHistory}>D2 히스토리 바로가기</button>
                     </div>
                     <div className="schedule_list">
-                        <div className="schedule">
-                            <div>
-                                <div className="schedule_img"><img src={data.image}/></div>
-                                <div className="schedule_txt">
-                                    <p className="title">{data.title}</p>
-                                    <p className="desc">{data.text}</p>
+                        {datas.map((data, idx) => 
+                            <div className="schedule">
+                                <div>
+                                    <div className="schedule_img"><img src={data.image}/></div>
+                                    <div className="schedule_txt">
+                                        <p className="title">{data.title}</p>
+                                        <p className="desc">{data.text}</p>
+                                    </div>
+                                </div>
+                                <div className="date_info">
+                                    {data.date.map(v => 
+                                        <div>{v}</div>
+                                    )}
+                                    <a href={data.link}>바로가기</a>
                                 </div>
                             </div>
-                            <div className="date_info">
-                                <p>20년 3월 18일 ~ 4월 2일 접수</p>
-                                <p>20년 5월 21일 ~ 5월 22일 핵데이</p>
-                                <p>바로가기</p>
-                            </div>
-                        </div>
+                        )}
                     </div>
                 </div>
-                
+            </div>
+
+            <div className="history_area">
+                <div className="history_section">
+                    <h1 align="center"> FOR DEVELOPERS, BY DEVELOPERS</h1>
+                    <p> 개발자들을 위한 탁월한 지식과 경험을 공유하며 함께 성장합니다! </p>
+                    <div className="history">
+                        <h2>History</h2>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- client/src/components/program/index.js
+++ client/src/components/program/index.js
@@ -46,7 +46,6 @@ const Program = () => {
     ]
 
     const scrollToTop = () => {
-        console.log('aa')
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
