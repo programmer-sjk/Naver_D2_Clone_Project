6ba8dceec732bc1dad8898f8b38fe4b074e7ec55
cameo29 <wodls0209@naver.com>
2020-04-03 00:13:46 +0900

    main css 수정, hello css 수정

--- client/src/components/navigation/index.js
+++ client/src/components/navigation/index.js
@@ -1,39 +1,50 @@
 import React from 'react';
-import {Route, Link} from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import 'css/App.css';
 
-import Main from 'components/main'
-import Hello from 'components/hello'
-import News from 'components/news'
-import Program from 'components/program'
-import About from 'components/about'
-
+import Main from 'components/main';
+import Hello from 'components/hello';
+import News from 'components/news';
+import Program from 'components/program';
+import About from 'components/about';
 
 const Navigator = () => {
-    return (
-        <div>    
-            <div className="header_section">
-                <ul className="navi_ul">
-                    <li><Link to="/"><img src="/images/top_logo.png" className="main_logo"/></Link></li>
-                    <li className="txt"><Link to="/hello">Hello world</Link></li>
-                    <li className="txt"><Link to="/news">D2 News</Link></li>
-                    <li className="txt"><Link to="/program">D2 Program</Link></li>
-                    <li className="txt"><Link to="/about">About D2</Link></li>
-                </ul>
-                <input type="text" placeholder="D2에서 검색" className="search_txt"/>
-                <div className="searchBox">
-                    <button className="searchBtn"></button>
-                </div>
-            </div>
-            <div>
-                <Route path="/" component={Main} exact={true}/>
-                <Route path="/hello" component={Hello}/>
-                <Route path="/news" component={News}/>
-                <Route path="/program" component={Program}/>
-                <Route path="/about" component={About}/>
-            </div>
+  return (
+    <div>
+      <div className="header_section">
+        <ul className="navi_ul">
+          <li>
+            <Link to="/">
+              <img src="/images/top_logo.png" className="main_logo" />
+            </Link>
+          </li>
+          <li className="txt">
+            <Link to="/hello">Hello world</Link>
+          </li>
+          <li className="txt">
+            <Link to="/news">D2 News</Link>
+          </li>
+          <li className="txt">
+            <Link to="/program">D2 Program</Link>
+          </li>
+          <li className="txt">
+            <Link to="/about">About D2</Link>
+          </li>
+        </ul>
+        <input type="text" placeholder="D2에서 검색" className="search_txt" />
+        <div className="searchBox">
+          <button className="searchBtn"></button>
         </div>
-    )
-}
+      </div>
+      <div>
+        <Route path="/" component={Main} exact={true} />
+        <Route path="/hello" component={Hello} />
+        <Route path="/news" component={News} />
+        <Route path="/program" component={Program} />
+        <Route path="/about" component={About} />
+      </div>
+    </div>
+  );
+};
 
 export default Navigator;
