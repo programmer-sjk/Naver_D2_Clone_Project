01e7ce6dc047bad145f496ac67e60e17e7a1bf2b
cameo29 <wodls0209@naver.com>
2020-04-06 22:10:34 +0900

    pagination 추가

--- client/src/components/main/containers/paginatebox.js
+++ client/src/components/main/containers/paginatebox.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import 'css/Main.css';
 
-const PaginateBox = () => {
+const PaginateBox = (props) => {
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
+  const pageCount = Math.ceil(itemsCount / pageSize);
+
+  if (pageCount === 1) {
+    return null;
+  }
+
+  const pages = [...Array(pageCount).keys()].map((i) => i + 1);
   return (
     <div class="paginate_box">
       <div class="paginate">
-        <a href="/home?page=0" class="btn_num select" data-number="0">
-          <span>1</span>
-        </a>
-        <a href="/home?page=1" class="btn_num" data-number="1">
-          <span>2</span>
-        </a>
-        <a href="/home?page=2" class="btn_num" data-number="2">
-          <span>3</span>
-        </a>
-        <a href="/home?page=3" class="btn_num" data-number="3">
-          <span>4</span>
-        </a>
-        <a href="/home?page=4" class="btn_num" data-number="4">
-          <span>5</span>
-        </a>
-        <a href="/home?page=5" class="btn_next">
-          <span title="이전">다음</span>
-        </a>
+        {pages.map((page) => (
+          <a
+            // href="/home?page={page}"
+            className={page === currentPage ? 'btn_num select' : 'btn_num'}
+            style={{ cursor: 'pointer' }}
+            data-number={page - 1}
+            onClick={() => onPageChange(page)}
+          >
+            <span>{page}</span>
+          </a>
+        ))}
       </div>
     </div>
   );
