f098b8b6e559fcbe81b6d64abd3544b0dafe5ce8
cameo29 <wodls0209@naver.com>
2020-04-15 22:58:18 +0900

    반응형 네비바 생성

--- client/src/components/navigation/index.js
+++ client/src/components/navigation/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route, Link } from 'react-router-dom';
 import 'css/App.css';
 
@@ -7,32 +7,48 @@ import Hello from 'components/hello';
 import News from 'components/news';
 import Program from 'components/program';
 import About from 'components/about';
-import Search from 'components/search'; 
-import SearchBtn from './searchBtn'
+import Search from 'components/search';
+import SearchBtn from './searchBtn';
 
 const Navigator = () => {
+  const [css, setCss] = useState(false);
+  const onChangeCss = () => {
+    setCss(!css);
+  };
   return (
     <div>
       <div className="header_section">
-        <ul className="navi_ul">
-          <li>
-            <Link to="/">
-              <img src="/images/top_logo.png" className="main_logo" alt=""/>
-            </Link>
-          </li>
-          <li className="txt">
-            <Link to="/hello">Hello world</Link>
-          </li>
-          <li className="txt">
-            <Link to="/news">D2 News</Link>
-          </li>
-          <li className="txt">
-            <Link to="/program">D2 Program</Link>
-          </li>
-          <li className="txt">
-            <Link to="/about">About D2</Link>
-          </li>
-        </ul>
+        <div className="logo_section">
+          <Link to="/">
+            <img src="/images/top_logo.png" className="main_logo" alt="" />
+          </Link>
+        </div>
+        <div className="navi">
+          <div
+            className={`btn_lnb_menu ${css ? 'open' : 'close'}`}
+            onClick={onChangeCss}
+          >
+            <a href="#">
+              <span className="blind">메뉴</span>
+            </a>
+          </div>
+          <div className={`ul_list ${css ? 'open' : 'close'}`}>
+            <ul className="navi_ul">
+              <li className="txt">
+                <Link to="/hello">Hello world</Link>
+              </li>
+              <li className="txt">
+                <Link to="/news">D2 News</Link>
+              </li>
+              <li className="txt">
+                <Link to="/program">D2 Program</Link>
+              </li>
+              <li className="txt">
+                <Link to="/about">About D2</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
         <SearchBtn></SearchBtn>
       </div>
       <div className="clear"></div>
--- client/src/components/navigation/searchBtn.js
+++ client/src/components/navigation/searchBtn.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import { withRouter  } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
-const SearchBtn = ({history}) => {
-    let param = null;
+const SearchBtn = ({ history }) => {
+  let param = null;
 
-    const search = () => {
-        history.push('/search?keyword=' + param)
-    }
+  const search = () => {
+    history.push('/search?keyword=' + param);
+  };
 
-    return (
-        <div>
-            <input type="text" placeholder="D2에서 검색" className="search_txt" onChange={(e) => param = e.target.value }/>
-            <div className="searchBox">
-                <button className="searchBtn" onClick={search}></button>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="search_input">
+      <input
+        type="text"
+        placeholder="D2에서 검색"
+        className="search_txt"
+        onChange={(e) => (param = e.target.value)}
+      />
+      <div className="searchBox">
+        <button className="searchBtn" onClick={search}></button>
+      </div>
+    </div>
+  );
+};
 
 export default withRouter(SearchBtn);
