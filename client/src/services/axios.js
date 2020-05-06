08358e9d26b8867e7e22c36801926488c702f748
sjk5766 <wjdrnr6662@gmail.com>
2020-05-06 14:41:13 +0900

    service에서 getAxios만 사용

--- client/src/services/axios.js
+++ client/src/services/axios.js
@@ -9,26 +9,3 @@ export const getAxios = async (url) => {
     console.log(err);
   }
 };
-
-export const search = async (keyword) => {
-  try {
-    const ret = await axios.get('/'); // 후에 keyword 추가
-  } catch (err) {
-    console.log(err);
-  }
-
-  // 현재 데이터가 없어서 빈 배열 리턴함.
-  return [];
-};
-
-export const abouts = async () => {
-  try {
-    const ret = await axios.get('http://localhost:8080/about');
-    return ret.data;
-  } catch (err) {
-    console.log(err);
-  }
-
-  // 현재 데이터가 없어서 빈 배열 리턴함.
-  return [];
-};
